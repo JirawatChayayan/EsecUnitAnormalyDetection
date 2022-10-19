320c14ca1c2de39023b73ca82416b634022170ed
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-19 13:04:26 +0000

    Adding tarfile member sanitization to extractall()

--- AIServer/AICore/data.py
+++ AIServer/AICore/data.py
@@ -48,7 +48,26 @@ class MVTecDataset:
             url = f"ftp://guest:GU.205dldo@ftp.softronics.ch/mvtec_anomaly_detection/{self.cls}.tar.xz"
             wget.download(url)
             with tarfile.open(f"{self.cls}.tar.xz") as tar:
-                tar.extractall(DATASETS_PATH)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, DATASETS_PATH)
             os.remove(f"{self.cls}.tar.xz")
             print("") # force newline
         else:
